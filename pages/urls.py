981623da62e63cc582892f178857924c48ea9f82
mamilkew <mui_milk@hotmail.com>
2018-06-01 16:46:31 +0700

    updated detail page. Clicking the title to see detail

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),  # ex: /pages/
+    path('<int:post_id>/', views.detail, name='detail'),  # ex: /pages/5/
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+# from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from .models import Post
 from django.http import HttpResponse
@@ -9,5 +10,13 @@ from django.http import HttpResponse
 
 def index(request):
     posts = Post.objects.filter(published_date__lte=timezone.now())
-
     return render(request, 'pages/index.html', {'posts': posts})
+
+
+def detail(request, post_id):
+    posts = get_object_or_404(Post, pk=post_id)
+    # try:
+    #     posts = Post.objects.get(pk=post_id)
+    # except Post.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    return render(request, 'pages/detail.html', {'posts': posts})
