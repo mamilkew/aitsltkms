833020ff85846bd0cfdff6ad7f35a5b112315e1f
mamilkew <mui_milk@hotmail.com>
2018-06-02 00:34:09 +0700

    Customized the admin change list following https://docs.djangoproject.com/en/2.0/intro/tutorial07/

--- pages/admin.py
+++ pages/admin.py
@@ -4,4 +4,13 @@ from .models import Post
 # Register your models here.
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # ...
+    list_display = ('title', 'graph', 'subject', 'published_date', 'was_published_recently')
+    list_filter = ['published_date']
+    search_fields = ['result']
+
+
+admin.site.register(Post, PostAdmin)
+
+
--- pages/models.py
+++ pages/models.py
@@ -1,7 +1,7 @@
 from django.contrib.postgres.fields import JSONField
 from django.db import models
 from django.utils import timezone
-
+import datetime
 
 # Create your models here.
 
@@ -38,6 +38,13 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.published_date <= now
+    was_published_recently.admin_order_field = 'published_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
     def __str__(self):
         return self.title
