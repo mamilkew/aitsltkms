4b5750600362971686e3d4c1e63a91158a117d20
mamilkew <mui_milk@hotmail.com>
2018-06-05 15:09:35 +0700

    Created Filter Facet following lists from the resuls -- PJyear, PJstatus. Next step is ajax to use filtering

--- pages/views.py
+++ pages/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 import os
 import json
 
+
 # Create your views here.
 
 
@@ -14,14 +15,14 @@ def index(request):
     return render(request, 'pages/index.html', {'posts': posts})
 
 
-def detail(request, post_id):
-    # posts = get_object_or_404(Post, pk=post_id)
+def filter_detail(request):
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url = os.path.join(SITE_ROOT, "static/data", "PJ_filter_PJyear.json")
     data = json.load(open(json_url))
     results = data['results']['bindings']
     new_results = []
 
+    # transform to pattern for visualization standard
     for result in results:
         tmp = {}
         tmp['subject'] = check_type(result.get('subject').get('type'), result.get('subject').get('value'))
@@ -30,17 +31,50 @@ def detail(request, post_id):
         new_results.append(tmp)
         # print(new_results)
 
+        return json.dumps({'status': 'OK', 'query': new_results})
+
+
+def detail(request, post_id):
+    # posts = get_object_or_404(Post, pk=post_id)
+
+    # SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    # json_url = os.path.join(SITE_ROOT, "static/data", "PJ_filter_PJyear.json")
+    # data = json.load(open(json_url))
+    # results = data['results']['bindings']
+    # new_results = []
+
     try:
         posts = Post.objects.get(pk=post_id)
+        results = posts.source['results']['bindings']
+        new_results = []
+        tmp_PJyear = []
+        tmp_PJstatus = []
+        # tmp_isRelatedTo = []
+
         # transform to pattern for visualization standard
+        for result in results:
+            tmp = {}
+            tmp['subject'] = check_type(result.get('subject').get('type'), result.get('subject').get('value'))
+            tmp['predicate'] = check_type(result.get('predicate').get('type'), result.get('predicate').get('value'))
+            tmp['object'] = check_type(result.get('object').get('type'), result.get('object').get('value'))
+            new_results.append(tmp)
+            # print(new_results)
+            if tmp['predicate'] == 'PJyear':
+                tmp_PJyear.append(tmp['object'])  # {'PJyear': [2013, 2014, 2015, 2016]}
+            elif tmp['predicate'] == 'PJstatus':
+                tmp_PJstatus.append(tmp['object'])
+            # elif tmp['predicate'] == 'isRelatedTo':
+            #     tmp_isRelatedTo.append(tmp['object'])
+
         posts.result = new_results
         posts.save()
-        #
+        filter_facets = {'PJyear': list_facet(tmp_PJyear),
+                         'PJstatus': list_facet(tmp_PJstatus)}  # ,'isRelatedTo': list_facet(tmp_isRelatedTo)
 
     except Post.DoesNotExist:
         raise Http404("Question does not exist")
 
-    return render(request, 'pages/detail.html', {'posts': posts})
+    return render(request, 'pages/detail.html', {'posts': posts, 'filter_facets': filter_facets})
 
 
 def check_type(type_result, value):
@@ -50,3 +84,8 @@ def check_type(type_result, value):
         return value
     else:
         return ''
+
+
+def list_facet(tmp_facets):
+    facets = sorted(list(set(tmp_facets)))
+    return facets
