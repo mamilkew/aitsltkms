512cff5581965eb63077b0f4175a458b6237f6ff
mamilkew <mui_milk@hotmail.com>
2018-06-14 15:12:18 +0700

    Updated dynamic nabbar depends on page which last published && duplicate(subject) using SQL injection-- following predicate value

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -4,9 +4,10 @@ from django.db.models import Avg, Count, Min, Max, Sum
 
 def navbarmain(request):
     from pages.models import Post
-    navbarMain = Post.objects.filter(published_date__lte=timezone.now())
-    # print(Post.objects.values('subject').annotate(newest_published_date=Max('published_date')))
-    pb = Post.objects.values('subject').annotate(newest_published_date=Max('published_date'))
-    pb_list = Post.objects.filter(subject__in=[b.get('subject') for b in pb], published_date__in=[b.get('newest_published_date') for b in pb])
+    raw_query = "SELECT DISTINCT ON (subject) *, max(published_date) FROM pages_post WHERE published_date is not null GROUP BY subject, id ORDER BY subject ASC , published_date DESC"
+    # for p in Post.objects.raw(raw_query):
+    #     print(p)
+    # pb = Post.objects.values('subject').annotate(newest_published_date=Max('published_date'))
+    # pb_list = Post.objects.filter(subject__in=[b.get('subject') for b in pb], published_date__in=[b.get('newest_published_date') for b in pb])
     # print(pb_list)
-    return {'navbarMain': pb_list}
+    return {'navbarMain': Post.objects.raw(raw_query)}
