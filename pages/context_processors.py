ee9b6b6919a101ccf0fbbeb7e2be57a1473e43ee
mamilkew <mui_milk@hotmail.com>
2018-07-05 22:11:21 +0700

    Navbar -- Updated Timeline link

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -10,4 +10,8 @@ def navbarmain(request):
     # pb = Post.objects.values('subject').annotate(newest_published_date=Max('published_date'))
     # pb_list = Post.objects.filter(subject__in=[b.get('subject') for b in pb], published_date__in=[b.get('newest_published_date') for b in pb])
     # print(pb_list)
-    return {'navbarMain': Postforcegraph.objects.raw(raw_query)}
+
+    from pages.models import Timelinegraph
+    timeline_bar = Timelinegraph.objects.filter(published_date__lte=timezone.now()).order_by('published_date').last()
+
+    return {'navbarMain': Postforcegraph.objects.raw(raw_query), 'navbar_timeline': timeline_bar}
