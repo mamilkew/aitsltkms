f18076a54b432cf5a1fef9f60ffd58032eec3de7
mamilkew <mui_milk@hotmail.com>
2018-07-16 20:51:30 +0700

    Navbar -- updated links of each page for Node-Links and Timeline

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -3,8 +3,15 @@ from django.db.models import Avg, Count, Min, Max, Sum
 
 
 def navbarmain(request):
-    from pages.models import Postforcegraph
-    raw_query = "SELECT DISTINCT ON (subject) *, max(published_date) FROM pages_postforcegraph WHERE published_date is not null GROUP BY subject, id ORDER BY subject ASC , published_date DESC"
+    # from pages.models import Postforcegraph
+    from pages.models import Forcegraph
+    raw_query = "SELECT DISTINCT ON (page_title) " \
+                "*, max(published_date) FROM pages_forcegraph " \
+                "WHERE published_date is not null " \
+                "GROUP BY page_title, id " \
+                "ORDER BY page_title ASC, published_date DESC"
+
+    # raw_query = "SELECT DISTINCT ON (subject) *, max(published_date) FROM pages_postforcegraph WHERE published_date is not null GROUP BY subject, id ORDER BY subject ASC , published_date DESC"
     # for p in Post.objects.raw(raw_query):
     #     print(p)
     # pb = Post.objects.values('subject').annotate(newest_published_date=Max('published_date'))
@@ -12,6 +19,6 @@ def navbarmain(request):
     # print(pb_list)
 
     from pages.models import Timelinegraph
-    timeline_bar = Timelinegraph.objects.filter(published_date__lte=timezone.now()).order_by('published_date').last()
+    timeline_bar = Timelinegraph.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
-    return {'navbarMain': Postforcegraph.objects.raw(raw_query), 'navbar_timeline': timeline_bar}
+    return {'navbarMain': Forcegraph.objects.raw(raw_query), 'navbar_timeline': timeline_bar}
